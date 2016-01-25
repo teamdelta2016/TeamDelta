aa43df27cdd5ec153c84415a8d02397b8547ec50
Sam Snyder <samsnyder96@gmail.com>
2016-01-25 18:28:49 +0000

    test2

--- peter/src/main/java/uk/ac/cam/teamdelta/peter/Test.java
+++ peter/src/main/java/uk/ac/cam/teamdelta/peter/Test.java
@@ -5,7 +5,8 @@ import uk.ac.cam.teamdelta.Constants;
 public class Test {
 
     public static void main(String[] args){
-        System.out.println(Constants.appName);
+        System.out.println("Test: " + Constants.appName);
+
     }
 
 }
