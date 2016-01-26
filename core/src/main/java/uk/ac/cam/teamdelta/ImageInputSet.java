a60249d27b5b7d59c77dc6c1e2871c568c882612
Sam Snyder <samsnyder96@gmail.com>
2016-01-26 11:25:51 +0000

    changed image sets

--- core/src/main/java/uk/ac/cam/teamdelta/ImageInputSet.java
+++ core/src/main/java/uk/ac/cam/teamdelta/ImageInputSet.java
@@ -1,22 +1,23 @@
 package uk.ac.cam.teamdelta;
-import java.awt.Image;
+
+import java.awt.image.BufferedImage;
 
 public class ImageInputSet
 {
-	public ImageInputSet
-	(
-		Image frontLeft_,
-		Image frontRight_,
-		Image left_,
-		Image right_,
-		Image back_
-	)
-	{
-		frontLeft = frontLeft_;
-		frontRight = frontRight_;
-		left = left_;
-		right = right_;
-		back = back_;
-	}
-	public final Image frontLeft, frontRight, left, right, back;
+    public ImageInputSet(
+         BufferedImage frontLeft_,
+         BufferedImage frontRight_,
+         BufferedImage left_,
+         BufferedImage right_,
+         BufferedImage back_
+         )
+    {
+        frontLeft = frontLeft_;
+        frontRight = frontRight_;
+        left = left_;
+        right = right_;
+        back = back_;
+    }
+
+    public final BufferedImage frontLeft, frontRight, left, right, back;
 }
