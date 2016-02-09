ae1d339eb870f622870b3252e5c1ba49f3984b47
Dgleish <amd96@cam.ac.uk>
2016-02-09 13:38:21 +0000

    Added new slider

--- larry/src/main/java/uk/ac/cam/teamdelta/larry/ParameterScreenController.java
+++ larry/src/main/java/uk/ac/cam/teamdelta/larry/ParameterScreenController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import uk.ac.cam.teamdelta.ImageParams;
+import uk.ac.cam.teamdelta.ImageProcParams;
 
 import java.io.IOException;
 
@@ -14,7 +15,7 @@ public class ParameterScreenController implements ScreenController {
     ScreensContainer container;
 
     @FXML
-    Slider slider1, slider2, slider3;
+    Slider slider1, slider2, slider3, slider4;
 
     @FXML
     ImageView view;
@@ -30,7 +31,10 @@ public class ParameterScreenController implements ScreenController {
     private void handleNextAction(ActionEvent event) throws IOException {
         LarrySettings.getInstance().setParameters(slider1.getValue(),
                 slider2.getValue(),
-                slider3.getValue());
+                slider3.getValue(),
+                slider4.getValue(),
+                // headlights boolean
+                true);
         container.nextScreen();
     }
 
@@ -45,7 +49,9 @@ public class ParameterScreenController implements ScreenController {
         // save the state of the sliders
         LarrySettings.getInstance().setParameters(slider1.getValue(),
                 slider2.getValue(),
-                slider3.getValue());
+                slider3.getValue(),
+                slider4.getValue(),
+                true);
         container.prevScreen();
     }
 
@@ -60,6 +66,7 @@ public class ParameterScreenController implements ScreenController {
         slider1.setValue(100.0);
         slider2.setValue(0);
         slider3.setValue(10);
+        slider4.setValue(30);
     }
 
     /**
@@ -73,6 +80,7 @@ public class ParameterScreenController implements ScreenController {
         slider1.setValue(50);
         slider2.setValue(67);
         slider3.setValue(80);
+        slider4.setValue(20);
     }
 
     /**
@@ -86,6 +94,7 @@ public class ParameterScreenController implements ScreenController {
         slider1.setValue(30);
         slider2.setValue(11);
         slider3.setValue(0);
+        slider4.setValue(100);
     }
 
 
@@ -97,10 +106,11 @@ public class ParameterScreenController implements ScreenController {
     @Override
     public void setupScreen() {
         // retrieve previous settings
-        ImageParams p = LarrySettings.getInstance().getParameters();
-        slider1.setValue(p.getA());
-        slider2.setValue(p.getB());
-        slider3.setValue(p.getC());
+        ImageProcParams p = LarrySettings.getInstance().getParameters();
+        slider1.setValue(p.blurValue);
+        slider2.setValue(p.ghostFactor);
+        slider3.setValue(p.nightTimeFactor);
+        slider4.setValue(p.darkEdgesFactor);
 
         view.setImage(new Image("/uk.ac.cam.teamdelta.larry/images/test1out.jpg"));
     }
