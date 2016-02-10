ff4b494e4e4143c38fbd59d9201b58191a15a18e
Michael Rawson <michael.rawson@ubuntu.com>
2016-02-10 11:15:36 +0000

    tied everything together -- can now load the right image for a given location in the UI

--- larry/src/main/java/uk/ac/cam/teamdelta/larry/UserInput.java
+++
@@ -1,5 +0,0 @@
-package uk.ac.cam.teamdelta.larry;
-
-public class UserInput {
-    //some class to represent the choice the user made
-}
--- robert/src/main/java/uk/ac/cam/teamdelta/robert/Frame.java
+++ robert/src/main/java/uk/ac/cam/teamdelta/robert/Frame.java
@@ -1,17 +1,16 @@
 package uk.ac.cam.teamdelta.robert;
 
-import uk.ac.cam.teamdelta.ImageInputSet;
+import uk.ac.cam.teamdelta.ImageOutputSet;
 import uk.ac.cam.teamdelta.JunctionInfo;
 
-public class Frame
-{
-	public Frame(ImageInputSet set, JunctionInfo junctions)
-	{
-		m_set = set;
-		m_junctions = junctions;
-	}
-	public ImageInputSet getImages() {return m_set;}
-	//public JunctionInfo numJunctions() {return m_junctions;}
-	private final ImageInputSet m_set;
-	private final JunctionInfo m_junctions;
+public class Frame {
+    public Frame(ImageOutputSet set, JunctionInfo junctions) {
+        m_set = set;
+        m_junctions = junctions;
+    }
+    public ImageOutputSet getImages() {
+        return m_set;
+    }
+    private final ImageOutputSet m_set;
+    private final JunctionInfo m_junctions;
 }
