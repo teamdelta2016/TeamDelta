8e80ba366a1d510466afd1eda59a096eea46187a
Michael Rawson <michael.rawson@ubuntu.com>
2016-02-16 16:21:25 +0000

    tidied the engine implementation

--- core/src/main/java/uk/ac/cam/teamdelta/Constants.java
+++ core/src/main/java/uk/ac/cam/teamdelta/Constants.java
@@ -2,4 +2,6 @@ package uk.ac.cam.teamdelta;
 
 public class Constants {
     public static final String appName = "Drive by Age";
+    public static final int pictureWidth = 640;
+    public static final int pictureHeight = 480;
 }
